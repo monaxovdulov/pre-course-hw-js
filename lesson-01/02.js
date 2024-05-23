c262669eaf0db376e52f0415b050c196bfd9dd5d
Roman <35300872+monaxovdulov@users.noreply.github.com>
2024-05-24 01:48:27 +0300

    fix

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,8 +1,8 @@
 let number1 = Math.floor(Math.random() * 100)
 
 if (number1%2 == 0) {
-    console.log(`${number1} :Это четное число`)
+    console.log(`${number1} :Это четное число`);
 }
 else {
-    console.log(`${number1} :Это нечетное число`)
+    console.log(`${number1} :Это нечетное число`);
 }
